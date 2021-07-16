814db8bb97245f4dc298d54299fbef567e96e60f
Demask90 <84012055+Demask90@users.noreply.github.com>
2021-07-16 22:53:49 +0200

    fine

--- mail/js/main.js
+++ mail/js/main.js
@@ -2,29 +2,54 @@
 
 //Chiedi all’utente la sua email,
 
-var email_utente = prompt("inserisci la tua mail")
+function Accedi () {
+    
+    var email_utente = document.getElementById('accedi').value;
+
+    //controlla che sia nella lista di chi può accedere,
+
+    for (var i = 0; i < caselle_mail.length; i++) {
+        console.log(caselle_mail[i]);
+        console.log(i);
+    
+        //stampa un messaggio appropriato sull’esito del controllo.
+        
+        if (email_utente == caselle_mail[i])  {
+            alert("Accesso eseguito correttamente"); break;
+            
+        } 
+    }
+    
+    if (!email_utente.includes("@gmail.com")) {
+        alert("La Email deve contenere @gmail.com");
+    
+    }    
+    
+    else if (email_utente !== caselle_mail[i]) {
+        alert ("Email non registrata");
+            
+    }        
+    
+}
 
 var caselle_mail = ["pippo@gmail.com", "pluto@gmail.com", "paperino@gmail.com", "topolino@gmail.com"]
 
-//controlla che sia nella lista di chi può accedere,
+function Registrazione() {
 
-for (var i = 0; i < caselle_mail.length; i++) {
-    console.log(caselle_mail[i]);
-    console.log(i);
+  email_utente = document.getElementById('registrati').value;
+  caselle_mail.push(email_utente);  
+  console.log(caselle_mail);
 
-    //stampa un messaggio appropriato sull’esito del controllo.
-    
-    if (email_utente == caselle_mail[i]) {
-        alert("Accesso eseguito correttamente"); break;
-    } 
-}
+  if (!email_utente.includes("@gmail.com")) {
+    alert("La Email deve contenere @gmail.com")
 
-if (!email_utente.includes("@gmail.com")) {
-    alert("La mail deve contenere @gmail.com");
-
+  } else
+   alert("Registrazione completata")
+   
 }    
 
-else if (email_utente !== caselle_mail[i]) {
-    alert ("Mail non registrata");
-        
-}        
+
+
+
+
+
